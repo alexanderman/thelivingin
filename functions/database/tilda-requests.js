ffbf2e4af3260fe92c09b34efb6e02d61b88fbdd
alexanderman <alexander.shnirman@gmail.com>
2019-10-14 23:41:01 +0300

    some notes

--- functions/database/tilda-requests.js
+++ functions/database/tilda-requests.js
@@ -3,10 +3,29 @@ const admin = require('firebase-admin');
 
 admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
-const tildaRequestsColl = db.collection('tilda-requests');
+const tildaRequestsColl = db.collection('dbg-tilda-requests'); // debug
 
 const saveRequest = requestBody => tildaRequestsColl.add({ ...requestBody, createdAt: new Date() });
 
 module.exports = {
     saveRequest
 };
+
+/**
+ * TODO:
+ *  - import helpers into firestore (create some mock data in db for tests)
+ * 
+ * User:
+ *  - id, email, name
+ *  - canHelp: bool
+ *  - helpDesc
+ *  - requests:[tildaRequestId]
+ *  - helps:[helpId]
+ * 
+ * Chat: 
+ *  - id
+ *  - createdBy: userId
+ *  - messages: [{ from, text, datetime }]
+ * 
+ * 
+ */
