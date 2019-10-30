d2bf5ce67f460a349fffee75d88868a34d0702c7
alexanderman <alexander.shnirman@gmail.com>
2019-10-30 22:30:03 +0200

    removed validators

--- functions/routes/tilda.js
+++ functions/routes/tilda.js
@@ -5,14 +5,23 @@ const express = require('express');
 const router = express.Router();
 
 const { requestValidator, helperValidator } = tildaValidators;
+const allowTildaTest = (req, res, next) => {
+    const data = [req.body = {}, req.cookies = {}, req.headers = {}, req.params = {}, req.query = {}];
+    const isTest = !!data.filter(item => item.test === 'test')[0];
 
-router.post('/request', requestValidator, (req, res) => {
+    if (isTest) {
+        return res.status(200).send('test');
+    }
+    return next();
+}
+
+router.post('/request', (req, res) => {
     requestService.registerRequest(req.body)
     .then(result => res.json(result))
     .catch(err => res.status(500).send(err));
 });
 
-router.post('/helper', helperValidator, (req, res) => {
+router.post('/helper', (req, res) => {
     helpersService.registerHelper(req.body)
     .then(result => res.json(result))
     .catch(err => res.status(500).send(err.message));
