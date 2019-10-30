c4e1959329f60ed96c55ac2818d66594d631211f
alexanderman <alexander.shnirman@gmail.com>
2019-10-30 22:37:08 +0200

    fixed tilda test + returned validators

--- functions/routes/tilda.js
+++ functions/routes/tilda.js
@@ -5,9 +5,10 @@ const express = require('express');
 const router = express.Router();
 
 const { requestValidator, helperValidator } = tildaValidators;
+
 const allowTildaTest = (req, res, next) => {
-    const data = [req.body = {}, req.cookies = {}, req.headers = {}, req.params = {}, req.query = {}];
-    const isTest = !!data.filter(item => item.test === 'test')[0];
+    const data = [req.body, req.cookies, req.headers, req.params, req.query];
+    const isTest = !!data.filter(item => (item || {}).test === 'test')[0];
 
     if (isTest) {
         return res.status(200).send('test');
@@ -15,13 +16,13 @@ const allowTildaTest = (req, res, next) => {
     return next();
 }
 
-router.post('/request', (req, res) => {
+router.post('/request', allowTildaTest, requestValidator, (req, res) => {
     requestService.registerRequest(req.body)
     .then(result => res.json(result))
     .catch(err => res.status(500).send(err));
 });
 
-router.post('/helper', (req, res) => {
+router.post('/helper', allowTildaTest, helperValidator, (req, res) => {
     helpersService.registerHelper(req.body)
     .then(result => res.json(result))
     .catch(err => res.status(500).send(err.message));
