e51bc85cc070cb89ad404546a8cdf287511c3668
alexanderman <alexanderman@users.noreply.github.com>
2019-10-31 12:11:33 +0200

    Client router (#2)
    * client: installed react-router-dom
    
    * router: public route + admin route

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,34 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import configureStore from './store'
 import './App.css';
 import { types as chatTypes, actions } from './store/redux/chatRedux';
 import Chat from './components/chat';
-
-
-function getUrlParams() {
-  return window.location.search.substring(1).split('&').reduce((acc, keyVal) => {
-    const key = keyVal.split('=')[0];
-    const val = decodeURIComponent(keyVal.split('=')[1]);
-    acc[key] = val;
-    return acc;
-  }, {});
-}
-const urlParams = getUrlParams();
+import Public from './public';
+import Admin from './admin';
 
 
 const store = configureStore();
-actions(store.dispatch).fetchChat(urlParams.chatId, urlParams.userId, urlParams.requestId, urlParams.sig);
-
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <Chat />
+        <Router>
+          <Switch>
+
+            <Route path="/Admin">
+              <Admin />
+            </Route>
+
+
+            <Route path="/">
+              <Public />
+            </Route>
+
+          </Switch>
+        </Router>
       </div>
     </Provider>
   );
