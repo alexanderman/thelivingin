610541208fe2528eac9f363089385670b29a49cb
alexanderman <alexander.shnirman@gmail.com>
2019-11-02 09:12:25 +0200

    added epics templates for admin

---
+++ client/src/store/epics/admin/usersEpic.js
@@ -0,0 +1,29 @@
+import { apiUrl } from '../../../config';
+import { ajax } from 'rxjs/ajax'
+import { of } from 'rxjs/index';
+import { ofType } from 'redux-observable';
+import { mergeMap, take, catchError } from 'rxjs/operators';
+
+import { types as usersTypes } from '../../redux/admin/usersRedux';
+
+
+export const fetchRequests = action$ => action$.pipe(
+    ofType(usersTypes.FETCH),
+    mergeMap(action => {
+        /** TODO: set correct url */
+        // const { payload: { chatId, userId, requestId, sig } } = action;
+        // console.log(`fetching user and chat data for ${chatId}; ${userId}; ${requestId}; ${sig}`);
+        // const url = `${apiUrl}/chats/${chatId}?userId=${userId}&requestId=${requestId}&sig=${sig}`;
+        // return ajax.getJSON(url);
+    }),
+    mergeMap(serverData => {
+        // const { user, chat, request, twilioToken } = serverData;
+        // const { _id: chatId, twilio: { sid } } = chat;
+        // return of(
+        //     { type: userTypes.INIT_USER, payload: { ...user, twilioToken } },
+        //     { type: chatTypes.FETCH_CHAT_SUCCESS, payload: { chat, request } },
+        //     { type: chatTypes.CONNECT_CHANNEL, payload: { chatId, channelSid: sid, twilioToken } },
+        // );
+    }),
+    catchError(err => of({ type: usersTypes.FETCH_ERROR, payload: `${err.message}; ${JSON.stringify(err.response)}` }))
+);
